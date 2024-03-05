d87f8094a5a678d637332061adce63ae457be4ac
tianopo <matheuslink18@hotmail.com>
2024-03-04 22:42:23 -0300

    consertado erro de compilação

--- src/routes/rotasApp.tsx
+++ src/routes/rotasApp.tsx
@@ -1,14 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Layout } from "src/componentes";
-import { Inicio } from "src/paginas/inicio/Inicio.pagina";
+import { Layout } from "src/components/Layout/Layout";
+import { Home } from "src/pages/home/Home.pagina";
 
 export const browserRouter = createBrowserRouter([
   {
     element: <Layout />,
     children: [
-      { path: "/inicio", element: <Inicio /> },
+      { path: "/inicio", element: <Home /> },
 
-      { path: "*", element: <Inicio /> },
+      { path: "*", element: <Home /> },
     ],
   },
 ]);
