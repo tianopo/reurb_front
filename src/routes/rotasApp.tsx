8c3d07e08f25f0507189e107a061ff368550cfdc
tianopo <matheuslink18@hotmail.com>
2024-03-13 12:36:18 -0300

    consertando rotas autenticadas

--- src/routes/rotasApp.tsx
+++ src/routes/rotasApp.tsx
@@ -1,12 +1,14 @@
-import { createBrowserRouter } from "react-router-dom";
+import { Navigate, createBrowserRouter } from "react-router-dom";
 import { LayoutX } from "src/components/Layout/LayoutX";
 import { Auth } from "src/pages/Auth/Auth.views";
 import { Home } from "src/pages/Home/Home.views";
 import { AutenticatedRoute } from "./AutenticatedRoute";
 
+const token = localStorage.getItem("token");
+
 export const browserRouter = createBrowserRouter([
-  { path: "/", element: <Auth /> },
-  { path: "*", element: <Auth /> },
+  { path: "/", element: token ? <Navigate to="/home" /> : <Auth /> },
+  { path: "*", element: <Navigate to="/home" /> },
   {
     element: <AutenticatedRoute />,
     children: [
