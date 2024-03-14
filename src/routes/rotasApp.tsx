74202cae20b0c239fc38b9ca52466b4d849da43c
tianopo <matheuslink18@hotmail.com>
2024-03-14 19:12:34 -0300

    mudança no nome da pasta

--- src/routes/rotasApp.tsx
+++ src/routes/rotasApp.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { LayoutX } from "src/components/Layout/LayoutX";
 import { Auth } from "src/pages/Auth/Auth.views";
-import { Perfil } from "src/pages/Home/Perfil.views";
+import { Perfil } from "src/pages/Perfil/Perfil.views";
 import { AutenticatedRoute } from "./AutenticatedRoute";
 import { PublicRoute } from "./PublicRoute";
 
