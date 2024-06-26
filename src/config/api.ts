1d83e6cb8994088149b7819877304888e5ab6a7a
tianopo <matheuslink18@hotmail.com>
2024-06-26 20:04:20 -0300

    conseguimos

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 const ip = process.env.REACT_APP_BACK_HOST;
-const port = process.env.REACT_APP_BACK_PORT;
+const port = process.env.REACT_APP_BACK_PORT ?? "";
 const timeOut = 1000 * 30;
 const authHeader = () => ({ authorization: `Bearer ${localStorage.getItem("token")}` });
 
