1920462d5e5ee52713c787cb622d55b038effe2b
tianopo <matheuslink18@hotmail.com>
2024-07-31 14:24:35 -0300

    titulo muda toda hora que a rota muda

--- src/hooks/utils/useTitle.tsx
+++ src/hooks/utils/useTitle.tsx
@@ -1,12 +1,15 @@
 import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import { app } from "src/routes/app";
 
 export const useTitle = () => {
+  const location = useLocation();
+
   useEffect(() => {
     const pathSnippets = location.pathname.split(app.auth).filter((i) => i);
     const pageTitle = pathSnippets.length > 0 ? pathSnippets[pathSnippets.length - 1] : "Login";
     const formattedTitle = `${pageTitle.charAt(0).toUpperCase()}${pageTitle.slice(1)}`;
 
     document.title = `${process.env.REACT_APP_PROJETO} - ${formattedTitle}`;
-  }, [location]);
+  }, [location.pathname]);
 };
